b1be3ab881768c2a505b8544f83e751a49908e43
Steffen Schwerdtfeger <88867126+StSchwerdtfeger@users.noreply.github.com>
2022-10-20 21:30:09 +0200

    Update RicoFilteringFunction.R

--- RicoFilteringFunction.R
+++ RicoFilteringFunction.R
@@ -3,11 +3,11 @@ https://journal.medicine.berlinexchange.de/pub/nqjpou17/release/1
 
 # Filtering function für Rico:
 
-filtering = function(x,y){ # x = id column vector; y = full data set
+filtering = function(x,y){ # x = id column vector; y = full data set (i.e., given class = data frame)
   library(dplyr) # load dplyr
   inter = duplicated(x)*1  # duplicated gives a logical, multiplied by 1
-                           # transforms it into a binary vector         
-  binded = as.data.frame(cbind(inter,x)) # bind id column and binary vector
+                           # and transforms it into a binary vector         
+  binded = as.data.frame(cbind(inter,x)) # bind id column and binary vector (column)
                                          # + change class to data frame,
                                          # otherwise upcoming dplyr filter 
                                          # function wont work:   
@@ -15,14 +15,14 @@ filtering = function(x,y){ # x = id column vector; y = full data set
   inter2=filter(binded, inter == "1" )   # filter rows with binary 0 :=
                                          # explicit non (!) duplicate values
   
-  blank = matrix(0,length(x))            # matrix vector list for the for loop:
+  blank = matrix(0,length(x))            # matrix vector list for the nested for loop:
   
   for(i in 1:length(inter2$x)){          # for each individual[i] value of inter2
                                          # i.e., each value with duplicates
      for(j in 1:length(x)){              # + for each respective duplicate
-                                         #  value in column vector with the ids
+                                         #  value in column vector with the ids,
        if(x[j] == inter2$x[i]){          # if i_dth value is equivalent to 
-                                         # the evaluated explicit duplcate id values
+                                         # the evaluated explicit duplicate id values
           blank[j] = 1  # then / in such a case assing a 1 to the redundant binary/logical column
        }
      }
@@ -32,13 +32,13 @@ filtering = function(x,y){ # x = id column vector; y = full data set
                                 # with the whole data frame that carries 
                                 # the id column
   data_frame = as.data.frame(data_frame) # turn it into a data frame
-  zzzz=filter(data_frame, blank != "1" ) # ... in order to finaly filter 
-                                         # all redundant id values, no 
-                                         # matter how many dubplicates there are,
+  filtered=filter(data_frame, blank != "1" ) # ... in order to finally filter 
+                                         # all (!) redundant id values, no 
+                                         # matter how many duplicates there are,
     #  in other words: if any id value had a duplicate, both the original 
     #  and duplicate were erased, due to the fact that column and row wise
-    #  relation cannot be evaluated unabivalently. 
-  return(zzzz) # return zzzz returns the values of the respective object, 
+    #  relation cannot be evaluated unambivalently. 
+  return(filtered) # return filtered returns the values of the respective object, 
                # i.e., the filtered full data frame
 } # End of function
   
